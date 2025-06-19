aebd7cd2066885cdf66be48f8688e1fdbbf4b390
辛广赫 <xinguanghe@msn.com>
2025-06-19 16:38:08 +0900

    Update Common.java
    Update Common.java

--- src/main/java/test/Common.java
+++ src/main/java/test/Common.java
@@ -29,7 +29,7 @@ import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
-
+import java.time.Duration;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
